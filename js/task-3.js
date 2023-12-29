5ad9200d4392a0f8bd77b128e10ed5f60fbecd9a
Dmytro Krasnikov <ted.dmutro@gmail.com>
2023-12-28 20:35:42 -0500

    hw1

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function getElementWidth(content, padding, border) {
+    const totalWidth = parseFloat(content) + 2 * (parseFloat(padding) + parseFloat(border));
+    return Math.round(totalWidth);
+  }
+  
+  console.log(getElementWidth("50px", "8px", "4px")); // 74
+  console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+  console.log(getElementWidth("200px", "0px", "0px")); // 200
+  
